7f9a6f30784c18541f074201b63656614a42c96b
shawag <727627411@qq.com>
2024-10-14 22:19:50 +0800

    rewrite the tb use constructed sim method

--- user/sim/Tb_Axi_Lite_Uart.sv
+++ user/sim/Tb_Axi_Lite_Uart.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
-module axilite_uart_tb();
+module Tb_AxizLite_Uart();
     reg             clock           ;
-    reg             async_resetn    ;
+    reg             reset    ;
     reg     [15:0]  s_axi_awaddr    ;
     reg             s_axi_awvalid   ;
     wire            s_axi_awready   ;
@@ -18,62 +18,133 @@ module axilite_uart_tb();
     wire    [1:0]   s_axi_rresp     ;
     wire            s_axi_rvalid    ;
     reg             s_axi_rready    ;
-    wire            interrupt       ;
+    //    wire            interrupt       ;
     wire            RTS;
+    reg             CTS;
 
-    // inner logic
-    // 10ns --> Freq = 100MHz
-    parameter       Clockperiod = 10;    
+     // inner logic
+     
     logic           start_write;
     logic           start_read;
     logic   [31:0]  rd_data;
-    logic           TxD_RxD;
+    wire            TxD_RxD;
+
+    parameter CLOCK_PERIOD = 10;
+    parameter P_FIFO_DEPTH = 8;
+    parameter P_S_AXI_DATA_WIDTH = 32;
+    parameter P_S_AXI_ADDR_WIDTH = 16;
 
 
-    Axi_Lite_Uart  #(
-        .P_S_AXI_DATA_WIDTH 	( 32        )  ,
-	    .P_S_AXI_ADDR_WIDTH 	( 16        )  
-    )
-    Axi_Lite_Uart_u0
-    (
-        .clock          (clock)         , 
-        .reset   (async_resetn)  ,
+    task clock_gen;
+    begin
+        clock = 1;
+        $display("%t:clock is activated, period is %d ns", $time,CLOCK_PERIOD);
+        forever # (CLOCK_PERIOD/2)
+            clock = ~clock;
+    end
+    endtask
 
-        .s_axi_awaddr   (s_axi_awaddr)  ,
-        .s_axi_awvalid  (s_axi_awvalid) ,
-        .s_axi_awready  (s_axi_awready) ,
+    task rst_gen;
+    input integer reset_time;
+    begin
+        reset = 1'b0;
+        $display("%t:reset low is activated", $time);
+        #(reset_time)
+        reset = 1'b1;
+        $display("%t:reset low is end, take %d ns to finish", $time, reset_time);
+    end
+    endtask
 
-        .s_axi_wdata    (s_axi_wdata)   ,
-        .s_axi_wvalid   (s_axi_wvalid)  ,
-        .s_axi_wready   (s_axi_wready)  ,
+    task sys_value_init;
+    begin
+        s_axi_awaddr    =   16'h0   ;
+        s_axi_awvalid   =   1'b0    ;
+        s_axi_wdata     =   32'h0   ;
+        s_axi_wvalid    =   1'b0    ;
+        s_axi_bready    =   1'b0    ;
+        s_axi_araddr    =   16'h0   ;
+        s_axi_arvalid   =   1'b0    ;
+        s_axi_rready    =   1'b0    ;
+        CTS             =   1'b0;
+    end
+    endtask
+   
+    task axi_lite_write_process;
+    input [P_S_AXI_ADDR_WIDTH-1:0] axi_write_addr;
+    input [P_S_AXI_DATA_WIDTH-1:0] axi_write_data;
+    begin
+        @(posedge clock)
+        s_axi_awvalid = 1'b1;
+        s_axi_awaddr = axi_write_addr;
+        s_axi_wdata = axi_write_data;
+        wait(s_axi_awready==1'b1);
+        @(posedge clock)
+        s_axi_awvalid = 1'b0;
+        @(posedge clock)
+        s_axi_wvalid = 1'b1;
+        s_axi_bready = 1'b1;
+        wait(s_axi_wready==1'b1);
+        @(posedge clock)
+        s_axi_wvalid = 1'b0;
+        wait(s_axi_bvalid==1'b1);
+        if(s_axi_bresp == 2'b00)
+            $display("%t:write data %h to address %h, write success", $time, axi_write_data, axi_write_addr);
+        else
+            $display("%t:write data %h to address %h, write failed", $time, axi_write_data, axi_write_addr);
+        @(posedge clock)
+        s_axi_bready = 1'b0; 
+    end
+    endtask
 
-        .s_axi_bresp    (s_axi_bresp)   ,
-        .s_axi_bvalid   (s_axi_bvalid)  ,
-        .s_axi_bready   (s_axi_bready)  ,
+    task axi_lite_read_process;
+    input [P_S_AXI_ADDR_WIDTH-1:0] axi_read_addr;
+    begin
+        @(posedge clock)
+        s_axi_arvalid = 1'b1;
+        s_axi_araddr = axi_read_addr;
+        wait(s_axi_arready==1'b1);
+        @(posedge clock)
+        s_axi_arvalid = 1'b0;
+        s_axi_rready = 1'b1;
+        wait(s_axi_rvalid==1'b1);
+        @(posedge clock)
+        rd_data = s_axi_rdata;
+        @(posedge clock)
+        $display("%t:read data %h from address %h", $time, rd_data, axi_read_addr);
+    end
+    endtask
 
-        .s_axi_araddr   (s_axi_araddr)  ,
-        .s_axi_arvalid  (s_axi_arvalid) ,
-        .s_axi_arready  (s_axi_arready) ,
+    initial begin
+        clock_gen;
+    end
 
-        .s_axi_rdata    (s_axi_rdata)   ,
-        .s_axi_rresp    (s_axi_rresp)   ,
-        .s_axi_rvalid   (s_axi_rvalid)  ,
-        .s_axi_rready   (s_axi_rready)  , 
+    initial begin
 
-       // .interrupt      (interrupt)     ,
-        .RxD            (TxD_RxD)           ,
-        .TxD            (TxD_RxD)           ,
-        .RTS           (RTS)              ,
-        .CTS           (1'b0)             
-    );
+        #(5*CLOCK_PERIOD)
+        sys_value_init;
+        rst_gen(10*CLOCK_PERIOD);
+        #(5*CLOCK_PERIOD)
+        axi_lite_write_process(16'h0004, 32'h00000001);
+        axi_lite_write_process(16'h0004, 32'h00000002);
+        axi_lite_write_process(16'h0004, 32'h00000003);
+        axi_lite_write_process(16'h0004, 32'h00000004);
+        axi_lite_write_process(16'h0004, 32'h00000005);
+        axi_lite_write_process(16'h0004, 32'h00000006);
+        axi_lite_write_process(16'h0004, 32'h00000007);
+        axi_lite_write_process(16'h0004, 32'h00000008);
+        #(10000*CLOCK_PERIOD)
+        axi_lite_read_process(16'h0000);
+    end
+    
 
 
     //  ============================= Control =============================
     //  100MHz Clock
+    /*
     initial                 clock = 1'b0       ;
     always #(Clockperiod/2) clock = ~clock;
-    
-
+    */
+    /*
     initial begin
         async_resetn    =   1'b0    ;
         /*      
@@ -86,7 +157,7 @@ module axilite_uart_tb();
         s_axi_araddr    =   16'h0   ;
         s_axi_arvalid   =   1'b0    ;
         s_axi_rready    =   1'b0    ;
-        */
+        
         #1500
         async_resetn    =   1'b1    ;
         //s_axi_bready    =   1'b1    ;
@@ -98,10 +169,12 @@ module axilite_uart_tb();
         #10
         start_write=0;
     end
+    */
 
     //  发起写请求
     //  写地址通道
     //  s_axi_awvalid
+    /*
     always_ff @(posedge clock, negedge async_resetn) begin
         if(!async_resetn) begin
             s_axi_awvalid <= 0;
@@ -213,12 +286,50 @@ module axilite_uart_tb();
             $strobe("%d",rd_data);
         end
     end
-
+    /*
     initial begin
         $dumpfile("./sim/axilite_uart_tb.vcd");
         $dumpvars(0, axilite_uart_tb);
         #10000000
         $finish;
     end
+    */
+    Axi_Lite_Uart  #(
+        .P_S_AXI_DATA_WIDTH 	( P_S_AXI_DATA_WIDTH        )  ,
+	    .P_S_AXI_ADDR_WIDTH 	( P_S_AXI_ADDR_WIDTH        )  ,
+        .P_FIFO_DEPTH           ( P_FIFO_DEPTH )
+    )
+    Axi_Lite_Uart_u0
+    (
+        .clock          (clock)         , 
+        .reset          (reset)  ,
+
+        .s_axi_awaddr   (s_axi_awaddr)  ,
+        .s_axi_awvalid  (s_axi_awvalid) ,
+        .s_axi_awready  (s_axi_awready) ,
+
+        .s_axi_wdata    (s_axi_wdata)   ,
+        .s_axi_wvalid   (s_axi_wvalid)  ,
+        .s_axi_wready   (s_axi_wready)  ,
+
+        .s_axi_bresp    (s_axi_bresp)   ,
+        .s_axi_bvalid   (s_axi_bvalid)  ,
+        .s_axi_bready   (s_axi_bready)  ,
+
+        .s_axi_araddr   (s_axi_araddr)  ,
+        .s_axi_arvalid  (s_axi_arvalid) ,
+        .s_axi_arready  (s_axi_arready) ,
+
+        .s_axi_rdata    (s_axi_rdata)   ,
+        .s_axi_rresp    (s_axi_rresp)   ,
+        .s_axi_rvalid   (s_axi_rvalid)  ,
+        .s_axi_rready   (s_axi_rready)  , 
+
+       // .interrupt      (interrupt)     ,
+        .RxD            (TxD_RxD)           ,
+        .TxD            (TxD_RxD)           ,
+        .RTS           (RTS)              ,
+        .CTS           (CTS)             
+    );
 
 endmodule
